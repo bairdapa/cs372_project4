c2f1cf626ec922c07bb19c21ca5d7cec66e74588
Xander <rednax825@gmail.com>
2020-08-02 18:15:55 -0700

    finished basic client

---
+++ client.py
@@ -0,0 +1,38 @@
+# This is the client application for project 4
+
+import socket
+import sys
+
+# create vars to store host info
+HOST = "127.0.0.1"
+PORT = 3304
+
+# create the socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to the server and print info
+client_socket.connect((HOST, PORT))
+print("connected to: " + HOST + ":" + str(PORT) + "\ntype /q to quit")
+    
+while 1:
+    # prompt for message to send
+    client_message = input(">")
+    
+    # check if the client quits
+    if client_message == "/q":
+        print("client quitting...")
+
+        # close socket and exit loop
+        client_socket.close()
+        break
+    
+    # send request to server
+    client_socket.sendall(str.encode(client_message))
+    
+    # receive and print the server's response
+    server_response = client_socket.recv(4096)
+    print(server_response.decode())
+
+# close socket and exit
+client_socket.close()
+sys.exit()
