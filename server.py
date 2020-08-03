cb7f62a69d12d8fc0d23fdfb88bfb36fbc53dbad
Xander <rednax825@gmail.com>
2020-08-02 18:16:09 -0700

    updated server to meet spec

--- server.py
+++ server.py
@@ -19,35 +19,34 @@ print("created server on " + HOST + ":" + str(PORT))
 # listen for incoming connections
 server_socket.listen(5)
 
+# accept a connection and store client info
+(client_socket, client_address) = server_socket.accept()
+
+# print info of client who just connected
+print("client connected: " + str(client_address) + "\ntype /q to quit")
+
 # loop to process requests as they come in
 while 1:
-    # accept a connection and store client info
-    (client_socket, client_address) = server_socket.accept()
-
-    # print info of client who just connected
-    print("client connected: " + str(address) + "\n")
 
     # read the message the client sent
     client_message = client_socket.recv(4096)
 
     # print the received message
-    print(str(address) + " says:")
-    print(client_message)
+    print(client_message.decode())
 
     # prompt for a reply
-    server_reply = input("\nwhat should the server say back?")
+    server_reply = input(">")
 
     # check for quit command
     if server_reply == "/q":
+        print("server quitting...")
+
         # close socket and exit loop
         client_socket.close()
         break
 
     # send reply
-    client_socket.sendall(server_reply)
-
-    # close socket to end connection
-    client_socket.close()
+    client_socket.sendall(str.encode(server_reply))
         
 # close server socket
 server_socket.close()
